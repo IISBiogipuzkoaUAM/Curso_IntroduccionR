2e622e4d7a769498ed1a43980c8c17cf43facf53
Lore Zumeta <lorezumeta@gmail.com>
2025-11-26 08:24:54 +0100

    add ejercicios.R

---
+++ Sesion2_Material/Ejercicios.R
@@ -0,0 +1,65 @@
+## ------------------------------------------------------------------------
+##
+## Curso:    Introducción a R en investigación biomédica
+## Sesion 2: Introducción a la bioestadística en R - PRÁCTICA
+##
+## ------------------------------------------------------------------------
+
+
+## Configuracion inicial: cargar paquetes etc.
+# install.packages("openxlsx")
+library(openxlsx)
+
+
+# Primera parte -----------------------------------------------------------
+
+## importar los datos de la bbdd rotterdam
+rotterdam <- XXX
+## Cuántas filas (observaciones) tiene?
+
+
+## Cuántas variables (columnas)?
+
+
+
+
+# |-- (a) El tamaño medio del tumor difiere de 25 mm ----------------------
+
+
+
+# |-- (b) Diferencia de edad terapia hormo vs no --------------------------
+## Hay alguna diferencia en la edad entre las pacientes que reciben terapia
+## hormonal y las que no
+
+
+
+# |-- (c) Tamaño tumoral terapia hormo vs no ------------------------------
+## La distribución del tamaño de los tumores difiere entre los grupos que
+## reciben terapia hormonal
+
+
+
+# |-- (d) Asociacion recurrencia y categorías de ganglios pos -------------
+## El estado de recurrencia está asociado con el número de categorías de
+## ganglios positivos.
+
+
+
+
+
+
+# Segunda parte -----------------------------------------------------------
+
+## importar datos
+rotterdam <- XXX
+
+
+# |-- (a) Modelo 1 --------------------------------------------------------
+
+
+# |-- (b) Modelo 2 --------------------------------------------------------
+
+
+# |-- (c) Modelo 3 --------------------------------------------------------
+
+
